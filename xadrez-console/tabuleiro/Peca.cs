19839884553aeb4648a831e7973bc5d3be8df77e
Joao Pedro VR <joaopedrovr91@gmail.com>
2016-10-17 08:58:59 -0200

    Testes inicias com movimentos possiveis das pecas

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -1,5 +1,5 @@
 ﻿namespace tabuleiro{
-    class Peca {
+    abstract class Peca {
 
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
@@ -15,5 +15,6 @@
         public void incremetarQteMovimento() {
             qteMovimentos++;
         }
+        public abstract bool[,] movimentosPossiveis();
     }
 }
